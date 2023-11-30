1e19b3d47db15f8006d84fb40faa700c296aef25
Pinky-Mona <150270126+Pinky-Mona@users.noreply.github.com>
2023-11-30 20:29:20 +0530

    Add files via upload

---
+++ backend/src/test/java/com/example/RegisterNdLogin/RegisterNdLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RegisterNdLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterNdLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
